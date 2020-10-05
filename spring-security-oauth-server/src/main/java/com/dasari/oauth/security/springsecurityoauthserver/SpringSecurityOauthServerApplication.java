056b6ab739a8da87788a850555fdb320da2f1864
dasari <dasari.4@gmail.com>
2020-10-05 10:56:43 -0500

    spring-security-oauth-client

---
+++ spring-security-oauth-server/src/main/java/com/dasari/oauth/security/springsecurityoauthserver/SpringSecurityOauthServerApplication.java
@@ -0,0 +1,14 @@
+package com.dasari.oauth.security.springsecurityoauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauthServerApplication.class, args);
+	}
+
+}
+
---
+++ spring-security-oauth-server/src/main/java/com/dasari/oauth/security/springsecurityoauthserver/config/ResourceServerConfig.java
@@ -0,0 +1,49 @@
+package com.dasari.oauth.security.springsecurityoauthserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .antMatchers("/private/**")
+                .authenticated();
+    }
+
+/*    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requestMatchers()
+                .antMatchers("/login","/oauth/authorize")
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.parentAuthenticationManager(authenticationManager)
+                .inMemoryAuthentication()
+                .withUser("Peter")
+                .password("Peter")
+                .roles("ADMIN");
+    }*/
+
+}
